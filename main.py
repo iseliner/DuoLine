f71359834f18cf44f2ec1d1d401f3c6e4165195a
iselin <iselin.erikssen@gmail.com>
2019-01-24 23:48:09 +0900

    can draw lines by making many polygons

--- main.py
+++ main.py
@@ -6,21 +6,25 @@ color, and together you can make pieces of art.
 
 from tkinter import *
 
-master = Tk()
-
 canvas_width = 600
 canvas_height = 500
+
+def paint(event):
+    python_color = "#000"
+    x1, y1 = (event.x - 1), (event.y - 1)
+    x2, y2 = (event.x + 1), (event.y + 1)
+    w.create_oval(x1, y1, x2, y2, fill=python_color)
+
+master = Tk()
+master.title("DuoLine")
 
 w = Canvas(master,
            height = canvas_height,
            width = canvas_width)
+w.pack(expand = YES, fill = BOTH)
+w.bind( "<B1-Motion>", paint )
 
-w.pack()
-
+w.create_text(30,15,text='DuoLine')
 w.create_rectangle(3,30,canvas_width,canvas_height, fill="#ffffff")
-
-w.create_text(30,
-              15,
-              text='DuoLine')
 
 mainloop()
