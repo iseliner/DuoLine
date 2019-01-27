736912cfad661ac3625392a6d85181f6b9095b38
iselin <iselin.erikssen@gmail.com>
2019-01-27 19:05:14 +0900

    pushing for safekeeping. added button

--- main.py
+++ main.py
@@ -15,8 +15,15 @@ def paint(event):
     x2, y2 = (event.x + 1), (event.y + 1)
     w.create_oval(x1, y1, x2, y2, fill=python_color)
 
+def resetPainting():
+    pass
+
 master = Tk()
 master.title("DuoLine")
+
+frame = Frame()
+drawing_frame = (frame)
+drawing_frame.place(x=3, y=30)
 
 w = Canvas(master,
            height = canvas_height,
@@ -24,7 +31,14 @@ w = Canvas(master,
 w.pack(expand = YES, fill = BOTH)
 w.bind( "<B1-Motion>", paint )
 
-w.create_text(30,15,text='DuoLine')
-w.create_rectangle(3,30,canvas_width,canvas_height, fill="#ffffff")
+btn_reset = Button(w, text="Reset", command=resetPainting())
+btn_reset.place(x=5, y=2)
+
+#draw_area = w.create_window(3, 30, height=canvas_height, width=canvas_width, anchor="nw")
+w.create_text(550, 15, text='DuoLine')
+w.create_rectangle(3, 30, canvas_width, canvas_height, fill="#ffffff")
 
 mainloop()
+
+#TODO: Make function for making lines
+#TODO: Make a maximum of two lines before changing "player"
---
+++ player.py
@@ -0,0 +1,7 @@
+
+class Player:
+    def __init__(self, color):
+        self.color = color
+
+    def makeline(self):
+        pass
